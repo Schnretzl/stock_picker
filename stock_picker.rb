b27b1a04fc7a20082dd06ef2d202e38a3c980e29
Wes Hendley <wrhendley@gmail.com>
2023-04-17 12:02:56 -0500

    Add logic for more edge cases.  Add more test cases

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,6 +1,7 @@
 def stock_picker(prices)
-    #code
-    # prices = [17,3,6,9,15,8,6,1,10]
+    if prices.length <= 1
+        return nil
+    end
     highest = []
     last_day = prices[prices.length - 1]
     while prices.length > 0 do
@@ -8,10 +9,38 @@ def stock_picker(prices)
         prices.shift
     end
     profit = highest.max
+    if profit <= 0
+        return nil
+    end
     buy_day = highest.index(profit)
     sell_day = highest.each_index.detect{ |index| index > buy_day && highest[index] == 0 }
-    # p sell_day
     [buy_day, sell_day]
 end
 
-p stock_picker([17,3,6,9,7,8,6,1,0])
+# Test case 1: Example input
+stock_prices = [7, 1, 5, 3, 6, 4]
+p stock_picker(stock_prices)  # [1, 4]
+
+# Test case 2: Prices increasing over time
+stock_prices = [1, 2, 3, 4, 5, 6]
+p stock_picker(stock_prices)  # [0, 5]
+
+# Test case 3: Prices decreasing over time
+stock_prices = [6, 5, 4, 3, 2, 1]
+p stock_picker(stock_prices)  # nil
+
+# Test case 4: Multiple peaks and valleys
+stock_prices = [1, 5, 2, 8, 3, 10]
+p stock_picker(stock_prices)  # [0, 5]
+
+# Test case 5: No profits possible
+stock_prices = [5, 4, 3, 2, 1]
+p stock_picker(stock_prices)  # nil
+
+# Test case 6: Empty array
+stock_prices = []
+p stock_picker(stock_prices)  # nil
+
+# Test case 7: Array with only one price
+stock_prices = [10]
+p stock_picker(stock_prices)  # nil
